7fefe1fec10c0af6cd5eefca69278236cb6ce194
seenso <>
2021-11-24 07:44:11 -0900

    Finished ProjectList.js, working on rendering ProjectItem

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+function ProjectList({ projects }) { //projects is arr of objs
+  // console.log("PROJECTS in ProjectList", projects);
+  // projects is arr of objs w id, name, about, technologies(arr)
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{
+        /* render ProjectItem components here */
+        projects.map(project => {
+          return <ProjectItem id={project.id} 
+                              name={project.name} 
+                              about={project.about} 
+                              techonlogies={project.technologies} />
+        })
+      }</div>
     </div>
   );
 }
